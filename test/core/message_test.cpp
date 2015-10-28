3eb869b4e6f427dff14b18f7433c1299da576076
Sebastián Mancilla <smancill@jlab.org>
2015-10-28 14:49:13 -0300

    Message: add basic unit test
    Test construction with a byte buffer.

---
+++ test/core/message_test.cpp
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=8 sts=4 et sw=4 tw=80: */
+
+#include "message.h"
+
+#include "gmock/gmock.h"
+
+using namespace testing;
+
+namespace {
+
+const xmsg::Topic topic = xmsg::Topic::raw("test/topic");
+
+}
+
+
+TEST(Message, CreateWithBytes)
+{
+    auto data = std::vector<std::uint8_t>{ 0x0, 0x1, 0x2, 0x3, 0xa, 0xb };
+    auto msg = xmsg::Message{topic, "test/binary", data};
+
+    EXPECT_THAT(msg.data(), ContainerEq(data));
+    EXPECT_THAT(msg.meta()->datatype(), StrEq("test/binary"));
+}
+
+
+int main(int argc, char *argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
