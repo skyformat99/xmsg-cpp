bc9ed4c191fe1d7aa0ba723353865192a484b94c
Sebastián Mancilla <smancill@jlab.org>
2015-11-04 18:29:11 -0300

    doc: add documentation for protobuf classes

--- src/proto/meta.h
+++ src/proto/meta.h
@@ -33,8 +33,32 @@ namespace xmsg {
 
 /**
  * Protobuf data classes and helpers.
+ * %xMsg uses [Protocol Buffers](https://developers.google.com/protocol-buffers)
+ * for storing and sending metadata, simple data and registration information
+ * between actors. The protobuf classes are defined in this namespace, along
+ * with some helper functions that simplify creating objects of those classes
+ * for common use cases.
  */
 namespace proto {
+
+/**
+ * \class Meta
+ * \brief Metadata for pub/sub communications.
+ *
+ * The fields in this protobuf class can be used to further describe
+ * the message data in pub/sub communications.
+ *
+ * The most important is `datatype`, which identifies the type of the
+ * serialized data. It is required for custom data objects, but it is set
+ * automatically if the message is created with \ref make_message.
+ *
+ * Most other fields are optional or reserved.
+ * `byteorder` must be set in case endiannes is important for serialization.
+ * `communicationid` can be set to identify messages.
+ * `replyto` is set by %xMsg to control sync communications.
+ * The reserved fields like `status`, `composition`, `action` and `control`
+ * should be set by higher-level layers.
+ */
 
 namespace internal {
 
@@ -55,11 +79,17 @@ inline void set_datatype(Meta& meta, const std::string& datatype)
 } // end namespace internal
 
 
+/**
+ * Creates an smart pointer to an empty %Meta object.
+ */
 inline std::unique_ptr<Meta> make_meta()
 {
     return std::make_unique<Meta>();
 }
 
+/**
+ * Creates an smart pointer to a copy of the given %Meta object.
+ */
 inline std::unique_ptr<Meta> copy_meta(const Meta& meta)
 {
     return std::make_unique<Meta>(meta);
