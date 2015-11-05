d5376449f1ddada8bc7b01877690a497d834a4a6
Sebastián Mancilla <smancill@jlab.org>
2015-11-04 23:43:59 -0300

    doc: add documentation for base data types

--- include/xmsg/constants.h
+++ include/xmsg/constants.h
@@ -75,31 +75,40 @@ const int registrar_port = 8888;
 
 /**
  * Identifiers for base data types.
+ * Most of these types represent the values that can be stored in
+ * proto::Data objects, and are set automatically
+ * when a message is created with \ref make_message.
+ * For more information about the Protocol Buffer value types, check
+ * [here](https://developers.google.com/protocol-buffers/docs/proto#scalar).
+ *
+ * Clients must define their own strings for custom data types.
+ * The identifier should be used to check that the message contains
+ * the expected data.
  */
 namespace mimetype {
 
-const std::string single_sint32 = "binary/sint32";
-const std::string single_sint64 = "binary/sint64";
-const std::string single_sfixed32 = "binary/sfixed32";
-const std::string single_sfixed64 = "binary/sfixed64";
-const std::string single_float = "binary/float";
-const std::string single_double = "binary/double";
-const std::string single_string = "text/string";
-const std::string bytes = "binary/bytes";
+const std::string single_sint32 = "binary/sint32";          ///< Signed integer. Uses variable-length encoding.
+const std::string single_sint64 = "binary/sint64";          ///< Signed integer. Uses variable-length encoding.
+const std::string single_sfixed32 = "binary/sfixed32";      ///< Signed integer. Always use 4 bytes.
+const std::string single_sfixed64 = "binary/sfixed64";      ///< Signed integer. Always use 8 bytes.
+const std::string single_float = "binary/float";            ///< Single precision floating point type.
+const std::string single_double = "binary/double";          ///< Double precision floating point type.
+const std::string single_string = "text/string";            ///< An UTF-8 encoded or 7-bit ASCII text.
+const std::string bytes = "binary/bytes";                   ///< An arbitrary sequence of bytes.
 
-const std::string array_sint32 = "binary/array-sint32";
-const std::string array_sint64 = "binary/array-sint64";
-const std::string array_sfixed32 = "binary/array-sfixed32";
-const std::string array_sfixed64 = "binary/array-sfixed32";
-const std::string array_float = "binary/array-float";
-const std::string array_double = "binary/array-double";
-const std::string array_string = "binary/array-string";
-const std::string array_bytes = "binary/array-bytes";
+const std::string array_sint32 = "binary/array-sint32";     ///< Repeated signed integers. Uses variable-length encoding.
+const std::string array_sint64 = "binary/array-sint64";     ///< Repeated signed integers. Uses variable-length encoding.
+const std::string array_sfixed32 = "binary/array-sfixed32"; ///< Repeated signed integers. Always use 4 bytes.
+const std::string array_sfixed64 = "binary/array-sfixed32"; ///< Repeated signed integers. Always use 8 bytes.
+const std::string array_float = "binary/array-float";       ///< Repeated single precision floating point types.
+const std::string array_double = "binary/array-double";     ///< Repeated double precision floating point types.
+const std::string array_string = "binary/array-string";     ///< Repeated UTF-8 encoded or 7-bit ASCII texts.
+const std::string array_bytes = "binary/array-bytes";       ///< Repeated arbitrary sequences of bytes.
 
-const std::string xmsg_data = "binary/native";
-const std::string java_object = "binary/java";
-const std::string cpp_object = "binary/cpp";
-const std::string python_object = "binary/python";
+const std::string xmsg_data = "binary/native";              ///< A serialized \ref xmsg::proto::Data "Data" object
+const std::string java_object = "binary/java";              ///< A serialized Java object
+const std::string cpp_object = "binary/cpp";                ///< A serialized C++ object
+const std::string python_object = "binary/python";          ///< A serialized Python object
 
 } // end namespace mime
 
