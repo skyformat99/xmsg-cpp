50221b45df6ef1cf196aa141792f27812eedd626
Sebastián Mancilla <smancill@jlab.org>
2016-08-09 13:52:12 -0400

    doc: link STL classes to cppreference.com

---
+++ doc/get_cppreference_tags.cmake
@@ -0,0 +1,4 @@
+message(STATUS "Downloading Doxygen tags from cppreference.com...")
+set(tags_url http://upload.cppreference.com/mwiki/images/f/f8/cppreference-doxygen-web.tag.xml)
+file(DOWNLOAD ${tags_url} ${DEST_DIR}/cppreference-doxygen-web.tag.xml)
+message(STATUS "Doxygen tags downloaded.")
