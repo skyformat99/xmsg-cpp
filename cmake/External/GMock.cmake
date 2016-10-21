90f8d2153e04e71d36b24014bb15f2a71a6c589b
Sebastián Mancilla <smancill@jlab.org>
2016-10-21 14:25:35 -0300

    cmake: update external GMock/GTest to version 1.8.0

--- cmake/External/GMock.cmake
+++ cmake/External/GMock.cmake
@@ -3,8 +3,8 @@ SET_DIRECTORY_PROPERTIES(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/ThirdParty)
 
 ExternalProject_Add(
   googlemock
-  URL https://googlemock.googlecode.com/files/gmock-1.7.0.zip
-  URL_HASH SHA1=f9d9dd882a25f4069ed9ee48e70aff1b53e3c5a5
+  URL https://github.com/google/googletest/archive/release-1.8.0.zip
+  URL_HASH SHA1=667f873ab7a4d246062565fad32fb6d8e203ee73
   TIMEOUT 30
   INSTALL_COMMAND ""
   LOG_DOWNLOAD ON
@@ -18,26 +18,26 @@ ExternalProject_Get_Property(googlemock SOURCE_DIR)
 ExternalProject_Get_Property(googlemock BINARY_DIR)
 
 # avoid error with set_target_properties
-file(MAKE_DIRECTORY "${SOURCE_DIR}/include")
-file(MAKE_DIRECTORY "${SOURCE_DIR}/gtest/include")
+file(MAKE_DIRECTORY "${SOURCE_DIR}/googlemock/include")
+file(MAKE_DIRECTORY "${SOURCE_DIR}/googletest/include")
 
 add_library(gmock IMPORTED STATIC IMPORTED)
 add_library(gtest IMPORTED STATIC IMPORTED)
 
 set_target_properties(gtest PROPERTIES
   IMPORTED_LOCATION
-    "${BINARY_DIR}/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    "${BINARY_DIR}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}"
   INTERFACE_INCLUDE_DIRECTORIES
-    "${SOURCE_DIR}/gtest/include"
+    "${SOURCE_DIR}/googletest/include"
   IMPORTED_LINK_INTERFACE_LIBRARIES
     "${CMAKE_THREAD_LIBS_INIT}"
 )
 
 set_target_properties(gmock PROPERTIES
   IMPORTED_LOCATION
-    "${BINARY_DIR}/${CMAKE_FIND_LIBRARY_PREFIXES}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    "${BINARY_DIR}/googlemock/${CMAKE_FIND_LIBRARY_PREFIXES}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}"
   INTERFACE_INCLUDE_DIRECTORIES
-    "${SOURCE_DIR}/include"
+    "${SOURCE_DIR}/googlemock/include"
   IMPORTED_LINK_INTERFACE_LIBRARIES
     gtest
 )
